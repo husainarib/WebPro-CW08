8a586dfec9755f5cd410ed3866ed057f2bd0b2c4
husainarib <husainarib@gmail.com>
2024-11-22 11:28:31 -0500

    Initial commit with Counter component

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import "./App.css";
+import HelloWorld from "./HelloWorld";
+import Counter from "./Counter";
+
+//TODO (FilteredList): create a produce list using the 'const' variable keyword
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <HelloWorld name={"[ARIB HUSAIN]"} />
+        <Counter/>
+        /*TODO
+        (FilteredList): create an instance of the FilteredList Component with a
+        variable named items that takes in your produce list*/
+      </div>
+    );
+  }
+}
+
+export default App;
