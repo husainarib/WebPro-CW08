b7448f5e128f38c37a8b1b12f433187f8ddfb5bd
husainarib <husainarib@gmail.com>
2024-11-22 12:03:29 -0500

    add dropdown menu to filter and search field for FilteredList.jsx

--- src/App.js
+++ src/App.js
@@ -2,18 +2,29 @@ import React, { Component } from "react";
 import "./App.css";
 import HelloWorld from "./HelloWorld";
 import Counter from "./Counter";
+import FilteredList from "./FilteredList";
 
-//TODO (FilteredList): create a produce list using the 'const' variable keyword
-
+const produce = [
+  { name: "Apple", type: "Fruit" },
+  { name: "Pineapple", type: "Fruit" },
+  { name: "Banana", type: "Fruit" },
+  { name: "Pear", type: "Fruit" },
+  { name: "Strawberry", type: "Fruit" },
+  { name: "Orange", type: "Fruit" },
+  { name: "Lettuce", type: "Vegetable" },
+  { name: "Cucumber", type: "Vegetable" },
+  { name: "Eggplant", type: "Vegetable" },
+  { name: "Squash", type: "Vegetable" },
+  { name: "Bell pepper", type: "Vegetable" },
+  { name: "Onion", type: "Vegetable" },
+];
 class App extends Component {
   render() {
     return (
       <div className="App">
         <HelloWorld name={"[ARIB HUSAIN]"} />
-        <Counter/>
-        /*TODO
-        (FilteredList): create an instance of the FilteredList Component with a
-        variable named items that takes in your produce list*/
+        <Counter />
+        <FilteredList items={produce} />
       </div>
     );
   }
--- src/FilteredList.jsx
+++ src/FilteredList.jsx
@@ -1,42 +1,51 @@
-import React, { Component } from 'react';
-import { DropdownButton, MenuItem } from 'react-bootstrap';
-import List from './List';
+import React, { Component } from "react";
+import { DropdownButton, DropdownItem } from "react-bootstrap";
+import List from "./List";
 
 class FilteredList extends Component {
   constructor(props) {
     super(props);
 
     //The state is just a list of key/value pairs (like a hashmap)
-    //TODO (FilteredList): Add an additional state variable within this.state called "type" and set it to a default value
     this.state = {
-      search: ""
+      search: "",
+      type: "All",
     };
   }
 
   //Sets the state whenever the user types on the search bar
   onSearch = (event) => {
-    this.setState({search: event.target.value.trim().toLowerCase()});
-  }
+    this.setState({ search: event.target.value.trim().toLowerCase() });
+  };
 
-  //TODO (FilteredList): Set the state of the "type" state variable depending on what is passed in
-  onFilter = (event) => {
-
-  }
-
-  //TODO (FilteredList): Change filterItem to take into account the "type" state variable when filtering
+  onFilter = (type) => {
+    this.setState({ type });
+  };
   filterItem = (item) => {
-      return item.name.toLowerCase().search(this.state.search) !== -1;
-  }
+    const matchesSearch = item.name.toLowerCase().includes(this.state.search);
+    const matchesType =
+      this.state.type === "All" || item.type === this.state.type;
 
-  render(){
+    return matchesSearch && matchesType;
+  };
+
+  render() {
     return (
-        <div className = "filter-list">
-         
-          /*TODO (FilteredList): Create a Dropdown Menu with three different menu options: Fruit, Vegetables, and All*/
-          
-          <input type = "text" placeholder = "Search" onChange = {this.onSearch} />
-          <List items = {this.props.items.filter(this.filterItem)} />
-        </div>
+      <div className="filter-list">
+        {/* Dropdown menu */}
+        <DropdownButton
+          id="typeDropdown"
+          title={`Filter: ${this.state.type}`}
+          onSelect={this.onFilter}
+        >
+          <DropdownItem eventKey="All">All</DropdownItem>
+          <DropdownItem eventKey="Fruit">Fruit</DropdownItem>
+          <DropdownItem eventKey="Vegetable">Vegetables</DropdownItem>
+        </DropdownButton>
+
+        <input type="text" placeholder="Search" onChange={this.onSearch} />
+        <List items={this.props.items.filter(this.filterItem)} />
+      </div>
     );
   }
 }
